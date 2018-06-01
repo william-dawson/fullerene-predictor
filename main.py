06a25c52f6a7f47ea4bd5200ec8dd706d87ed7f0
William Dawson <william.dawson@riken.jp>
2018-06-01 19:19:42 +0900

    Extra error info.

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from fullerene_curvature.fullerene import Fullerene
 if __name__ == "__main__":
     file_name = argv[1]
 
-    input_fullerene = Fullerene(file_name)
+    try:
+        input_fullerene = Fullerene(file_name)
 
-    k_values = compute_k_values(input_fullerene)
-    g_values = compute_g_values(input_fullerene)
+        k_values = compute_k_values(input_fullerene)
+        g_values = compute_g_values(input_fullerene)
 
-    euler_characteristic = compute_euler_characteristic(g_values)
-    energy_value = compute_energy(k_values, g_values)
+        euler_characteristic = compute_euler_characteristic(g_values)
+        energy_value = compute_energy(k_values, g_values)
 
-    print("Energy:", energy_value)
-    print("Euler_Characteristic:", euler_characteristic)
+        print("Energy:", energy_value)
+        print("Euler_Characteristic:", euler_characteristic)
+    else:
+        print("Failed:", file_name)

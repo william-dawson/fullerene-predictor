996a8d37f7dcc162077a3626436d2ba9eb73c373
William Dawson <william.dawson@riken.jp>
2021-01-29 17:06:13 +0900

    Removed depreciated feature.

--- main.py
+++ main.py
@@ -2,15 +2,11 @@
 '''
 from sys import argv
 from fullerene_curvature.curvature import compute_k_values, compute_g_values, \
-    compute_energy, compute_euler_characteristic, compute_bond_stress
+    compute_energy, compute_euler_characteristic
 from fullerene_curvature.fullerene import Fullerene
 
 if __name__ == "__main__":
     file_name = argv[1]
-    try:
-        site_output = argv[2]
-    except:
-        site_output = None
 
     try:
         input_fullerene = Fullerene(file_name)
@@ -27,9 +23,3 @@ if __name__ == "__main__":
     except:
         print("Failed:", file_name)
 
-    if site_output:
-        site_values = compute_bond_stress(input_fullerene, k_values, g_values)
-        if site_output == "Print":
-            print(site_values)
-        elif site_output == "Plot":
-            pass
